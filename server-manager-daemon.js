4763bd7504dcb30cb459c476365fc71d3aeccd29
Firaz Fhansurie <firazfhansurie@Firazs-MacBook-Air.local>
2026-01-31 19:18:15 +0800

    Add CORS support to server manager daemon

--- server-manager-daemon.js
+++ server-manager-daemon.js
@@ -6,10 +6,19 @@ const { exec } = require('child_process');
 const { promisify } = require('util');
 const path = require('path');
 const fs = require('fs').promises;
+const cors = require('cors');
 
 const execPromise = promisify(exec);
 const app = express();
 const PORT = 9000;
+
+// Enable CORS for all origins (since this is a management tool)
+app.use(cors({
+    origin: '*',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(express.json());
 app.use(express.static('public'));
